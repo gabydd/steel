401637f687cac95582d2ead410a93f8e2e2fd48d
github-action-benchmark <github@users.noreply.github.com>
2023-11-06 01:18:41 +0000

    add Benchmark (cargo) benchmark result for 45f582560445da5faaaa19098c5b50e7f872c10e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1699060513328,
+  "lastUpdate": 1699233521475,
   "repoUrl": "https://github.com/mattwparas/steel",
   "entries": {
     "Benchmark": [
@@ -93,6 +93,100 @@ window.BENCHMARK_DATA = {
             "name": "ackermann-3-3",
             "value": 119,
             "range": "± 4",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "mattwparas",
+            "username": "mattwparas",
+            "email": "matthewparas2020@u.northwestern.edu"
+          },
+          "committer": {
+            "name": "mattwparas",
+            "username": "mattwparas",
+            "email": "matthewparas2020@u.northwestern.edu"
+          },
+          "id": "45f582560445da5faaaa19098c5b50e7f872c10e",
+          "message": "use equality for functions that is compatible with rustc 1.65",
+          "timestamp": "2023-11-05T17:43:18Z",
+          "url": "https://github.com/mattwparas/steel/commit/45f582560445da5faaaa19098c5b50e7f872c10e"
+        },
+        "date": 1699233520394,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "range-big",
+            "value": 107,
+            "range": "± 8",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "map-big",
+            "value": 608743,
+            "range": "± 2375",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "transducer-map",
+            "value": 1474946,
+            "range": "± 16295",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "filter-big",
+            "value": 424079,
+            "range": "± 2873",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ten-thousand-iterations",
+            "value": 106,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ten-thousand-iterations-letrec",
+            "value": 106,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "trie-sort-without-optimizations",
+            "value": 33471,
+            "range": "± 1976",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "fib-28/fib-28",
+            "value": 58647553,
+            "range": "± 2469113",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "engine-creation",
+            "value": 41516397,
+            "range": "± 962893",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "register-fn",
+            "value": 213,
+            "range": "± 57",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "multiple-transducers",
+            "value": 8713,
+            "range": "± 24",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "ackermann-3-3",
+            "value": 107,
+            "range": "± 2",
             "unit": "ns/iter"
           }
         ]
